e85bbbf7128edd0ce991140a5104aa63bc7b9e1b
sergei Knatsko <sknatsko@gmail.com>
2024-10-10 18:52:15 -0400

    first commit

---
+++ exercise4.py
@@ -0,0 +1,4 @@
+lengthRoom=float(input("enter lenght in meters: "))
+widthRoom=float(input("enter width in meters: "))
+areaRoom=lengthRoom*widthRoom
+print("the area of the room is: ",areaRoom,"m²")
